5e116dedce3a438dd46910e068e420ca20129e52
und3f1n3dcoder <3133@online.dooninternational.net>
2025-04-28 13:47:52 +0530

    bombadrilo

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PDC/', // <-- REPLACE with your actual GitHub repo name!
+
 })
+
+// vite.config.js
